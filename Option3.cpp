1cbd70fced17bfc9b61f86d29fa05237de0a21e1
Josen <jiang891203@126.com>
2017-09-02 16:27:51 +0800

    Typo Issue.

--- Option3.cpp
+++ Option3.cpp
@@ -4,9 +4,9 @@ pthread_mutex_t Singleton3::m_Mutex = PTHREAD_MUTEX_INITIALIZER; //Initialize th
 
 Singleton3* Singleton3::GetInstance()
 {
-	pthread_mutex_lock(&m_Mutex); // This is need before C++Ox
+	pthread_mutex_lock(&m_Mutex); // This is need before C++OX
 	static Singleton3 l_Instance;
-	pthread_mutex_unlock(&m_Mutex); // This is need before C++Ox
+	pthread_mutex_unlock(&m_Mutex); // This is need before C++OX
 
 	return &l_Instance;
 }
