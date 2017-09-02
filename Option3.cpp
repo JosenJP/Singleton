da13694d89671f470550cbe75e2ec2b245ec6b9d
Josen <jiang891203@126.com>
2017-09-02 16:43:03 +0800

    Add Comments Detail

--- Option3.cpp
+++ Option3.cpp
@@ -4,9 +4,9 @@ pthread_mutex_t Singleton3::m_Mutex = PTHREAD_MUTEX_INITIALIZER; //Initialize th
 
 Singleton3* Singleton3::GetInstance()
 {
-	pthread_mutex_lock(&m_Mutex); // This is need before C++OX
+	pthread_mutex_lock(&m_Mutex); // This is need before C++11. C++11 make sure the constructor of static object is safe to multiple threads.
 	static Singleton3 l_Instance;
-	pthread_mutex_unlock(&m_Mutex); // This is need before C++OX
+	pthread_mutex_unlock(&m_Mutex); // This is need before C++11
 
 	return &l_Instance;
 }
